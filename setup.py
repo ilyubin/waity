4c97a89727822710c64cf0f9dac4774de19eeddc
igor.lyubin <ilyubin@inbox.ru>
2018-04-27 01:08:00 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@ from setuptools import setup
 
 setup(
     name='waity',
-    version='0.1',
-    description='The funniest joke in the world',
-    keywords='funniest joke comedy flying circus',
+    version='0.2',
+    description='wait any condition',
     url='http://github.com/ilyubin/waity',
+    download_url='https://github.com/peterldowns/mypackage/archive/0.2.tar.gz',
     author='Igor Lyubin',
     author_email='igor.lyubin@outlook.com',
     license='MIT',
     packages=['waity'],
     install_requires=[],
-    # test_suite='nose.collector',
     tests_require=['pytest'],
-    classifiers=[],
-    include_package_data=True,
-    zip_safe=False
+    keywords=['wait', 'sleep', 'timeout'],
 )
